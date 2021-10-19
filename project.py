b3536299d1141130db49acaae32432820f772d1e
ta-data-lis <lisbon-ta-team@ironhack.com>
2021-10-19 12:59:21 +0100

    conflicts resolved

--- project.py
+++ project.py
@@ -1,21 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e74195be947541e1ba315d345984a8b699a56679
+
 print("hello")
 
 
 print("Good Morning")
-<<<<<<< HEAD
+
 
 print('World')
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 print('World')
 
->>>>>>> e74195be947541e1ba315d345984a8b699a56679
+
 print("hello, how are you?")
 
 print("hello")
@@ -38,8 +32,4 @@ print("THE WIFI IS FAILING AND I NEED A CIGARETTE")
 
 print("Im here, finally!")
 
-<<<<<<< HEAD
->>>>>>> e0ac27686f9482ead3ddc374db6daccf5fe085df
->>>>>>> 1737073e4a6c9dd4b1d3dafcd85ca4ed0b874bf5
-=======
->>>>>>> e74195be947541e1ba315d345984a8b699a56679
+
